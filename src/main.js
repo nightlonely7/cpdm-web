597d44b3a7ae0582a7a01c402393c0c96764043a
MrQuang <QuangHVSE61073@fpt.edu.vn>
2019-04-17 19:54:58 +0700

    sad

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ import 'vuetify/dist/vuetify.min.css'
 import 'animate.css'
 import moment from 'moment'
 import CKEditor from '@ckeditor/ckeditor5-vue';
+import './assets/ckeditor.css';
 
 Vue.config.productionTip = false;
 
@@ -28,7 +29,7 @@ Vue.use(CKEditor);
 
 axios.interceptors.request.use(
     (config) => {
-        // console.log(config);
+        console.log(config);
         let token = localStorage.getItem('cpdm_token');
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
